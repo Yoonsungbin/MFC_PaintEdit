6d5ee04cb3ff018ad30bcf5aa739583a7eb808c8
Yoonsungbin <freelifestyle2550@gmail.com>
2015-05-10 18:02:32 +0900

    PaintEdit 프로젝트 생성

---
+++ PaintEdit/PaintEdit/PaintEditView.h
@@ -0,0 +1,46 @@
+
+// PaintEditView.h : CPaintEditView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CPaintEditView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CPaintEditView();
+	DECLARE_DYNCREATE(CPaintEditView)
+
+// 특성입니다.
+public:
+	CPaintEditDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 구현입니다.
+public:
+	virtual ~CPaintEditView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // PaintEditView.cpp의 디버그 버전
+inline CPaintEditDoc* CPaintEditView::GetDocument() const
+   { return reinterpret_cast<CPaintEditDoc*>(m_pDocument); }
+#endif
+
