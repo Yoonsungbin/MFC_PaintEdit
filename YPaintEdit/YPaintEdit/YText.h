48de5c5ecba7b80e3d2237ff75ceee3669b1731c
khsoon <pcoli@naver.com>
2015-05-26 14:19:57 +0900

    YText 클래스 설계

--- YPaintEdit/YPaintEdit/YText.h
+++ YPaintEdit/YPaintEdit/YText.h
@@ -1,14 +1,39 @@
 #pragma once
 #include "YObject.h"
-class YText :
-	public YObject
+class YText : public YObject
 {
 public:
+	// Constructor, Destructor
 	YText();
 	~YText();
 
-	virtual void moveAll() = 0;
-	virtual void deleteAll() = 0;
-	virtual void draw(CDC*) = 0;
-};
+	// Virtual
+	virtual void moveAll();
+	virtual void deleteAll();
+	virtual void draw(CDC*);
 
+	// Setter
+	void changePoint(CPoint nPoint);
+	void changeText(CString nText);
+	void changeFont(CFont nFont);
+	void changeFontColor(COLORREF nColor);
+	void changeBkColor(COLORREF nColor);
+	void changeFontSize(int nFontSize);
+
+	// Getter
+	CPoint getPoint();
+	CString getText();
+	CFont getFont();
+	COLORREF getFontColor();
+	COLORREF getBkColor();
+	int getFontSize();
+
+	// Variables
+private:
+	CPoint point;
+	CString text;
+	CFont font;
+	COLORREF fontColor;
+	COLORREF bkColor;
+	int fontSize;
+};
